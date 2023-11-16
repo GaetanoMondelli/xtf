14a48a99c10f0b4348b5866cdacf74e211d33fd4
Gaetano Mondelli <gaetano@aikon.com>
2023-11-16 01:38:25 +0000

    fix: refactor deployment script for chain selector id and tested UI

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -11,13 +11,16 @@ async function main() {
   const etfContractName = "ETFv2";
   const fungibleTokenName = "FungibleToken";
   const priceAggregatorContractName = "MockAggregator";
+  const mockRouterContractName = "MockRouterClient";
   const royaltyBps = 1000;
   const ETFURI = "https://etfx.com";
   const contracts: any = {};
   const DEMO_USER_ADDRESS = "0x2a1F5eB3e84e58e6F1e565306298B9dE1273f203";
+  const mockChainSelectorId = 0;
 
 
   const contractNamesDeployParams: any = [
+    [mockRouterContractName],
     [fungibleTokenName, "TokenToWrapped1", "TW1"],
     [fungibleTokenName, "TokenToWrapped2", "TW2"],
     [nativeWrapperContractName, "NativeTokenWrapper", "NTW"],
@@ -27,18 +30,22 @@ async function main() {
     [etfTokenContractName],
     (owner: any, nativeTokenWrapper = contracts[nativeWrapperContractName][0],
       etfTokenContract = contracts[etfTokenContractName][0],
+      router = contracts[mockRouterContractName][0],
       tokenAmounts = [
         {
+          chainIdSelector: mockChainSelectorId,
           assetContract: nativeAddress,
           amount: ethers.utils.parseEther("0.5"),
           oracleAddress: contracts[priceAggregatorContractName][0].address,
         },
         {
+          chainIdSelector: mockChainSelectorId,
           assetContract: contracts[fungibleTokenName][0].address,
           amount: 10,
           oracleAddress: contracts[priceAggregatorContractName][1].address,
         },
         {
+          chainIdSelector: mockChainSelectorId,
           assetContract: contracts[fungibleTokenName][1].address,
           amount: 20,
           oracleAddress: contracts[priceAggregatorContractName][2].address,
@@ -47,14 +54,18 @@ async function main() {
     ) => [etfContractName,
         "ETF-v0.0.1",
         "ETF",
-        owner.address,
-        royaltyBps,
+        {
+          recipient: owner.address,
+          bps: royaltyBps
+        },
         nativeTokenWrapper.address,
         etfTokenContract.address,
         etfTokenPerWrap,
         fee,
         tokenAmounts,
         ETFURI,
+        mockChainSelectorId,
+        router.address
       ]
   ]
 
@@ -90,8 +101,8 @@ async function main() {
 
   // mint some fungible tokens to the demo user
   const decimals = await contracts[fungibleTokenName][0].decimals();
-  await contracts[fungibleTokenName][0].mint(DEMO_USER_ADDRESS, BigInt(100*10**decimals));
-  await contracts[fungibleTokenName][1].mint(DEMO_USER_ADDRESS, BigInt(100*10**decimals));
+  await contracts[fungibleTokenName][0].mint(DEMO_USER_ADDRESS, BigInt(100 * 10 ** decimals));
+  await contracts[fungibleTokenName][1].mint(DEMO_USER_ADDRESS, BigInt(100 * 10 ** decimals));
 
   writeFileSync("CONTRACTS.json", JSON.stringify(contracts, null, 2));
 }
